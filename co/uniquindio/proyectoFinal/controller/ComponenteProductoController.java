42b1df6bc1c12fb2da7a50e210599a27d7729f95
JuanEstebanOsma1012 <osmajuan760@gmail.com>
2022-06-02 16:39:47 -0500

    nueva vista añadida

---
+++ co/uniquindio/proyectoFinal/controller/ComponenteProductoController.java
@@ -0,0 +1,31 @@
+package co.uniquindio.proyectoFinal.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ComponenteProductoController {
+
+    @FXML
+    private ImageView imgProducto;
+
+    @FXML
+    private Label txtDescripcionProducto;
+
+    @FXML
+    private Label txtNombreProducto;
+
+    @FXML
+    void agregarAlCarrito(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        
+    	
+    	
+    }
+}
+
--- co/uniquindio/proyectoFinal/controller/ProductComponentController.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.tiendaQuindio.controller;
-
-public class ProductComponentController {
-
-}
