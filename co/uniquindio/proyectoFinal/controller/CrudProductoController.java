8931386e78fa454bfe2759f1b4aaad4369e9343c
Juan Esteban Parra Parra <juane.parrap@uqvirtual.edu.co>
2022-06-07 23:53:07 -0500

    Correccion excepciones en CrudProductoController

--- co/uniquindio/proyectoFinal/controller/CrudProductoController.java
+++ co/uniquindio/proyectoFinal/controller/CrudProductoController.java
@@ -2,6 +2,7 @@ package co.uniquindio.proyectoFinal.controller;
 
 import co.uniquindio.proyectoFinal.exceptions.NegativeNumberException;
 import co.uniquindio.proyectoFinal.exceptions.ProductoException;
+import co.uniquindio.proyectoFinal.exceptions.SedeException;
 import co.uniquindio.proyectoFinal.exceptions.StringNuloOrVacioException;
 import co.uniquindio.proyectoFinal.model.Administrador;
 import co.uniquindio.proyectoFinal.model.Empresa;
@@ -24,24 +25,24 @@ import javafx.scene.image.Image;
 
 public class CrudProductoController {
 
-	Empresa empresa = Singleton.getInstance().getEmpresa();
-	Administrador administrador;
-	Producto selectedItemTable;
-	Producto selectedItemList;
-	ObservableList<Producto> listaProductosData = FXCollections.observableArrayList();
+    Empresa empresa = Singleton.getInstance().getEmpresa();
+    Administrador administrador;
+    Producto selectedItemTable;
+    Producto selectedItemList;
+    ObservableList<Producto> listaProductosData = FXCollections.observableArrayList();
 
     @FXML
     private TextField txtCodigoProductoBuscar;
 
     @FXML
     private TextField txtCantidadProducto;
-    
+
     @FXML
     private TextArea txtDescripcionProducto;
-    
+
     @FXML
     private TextField txtNombreImagenProducto;
-    
+
     @FXML
     private TableColumn<Producto, CategoriaProducto> tblColTipoProducto;
 
@@ -74,171 +75,176 @@ public class CrudProductoController {
 
     @FXML
     private TableView<Producto> tblProducto;
-    
+
     @FXML
     private Label mensajeError;
-    
+
     @FXML
     private ListView<Producto> listProductosEmpresa;
-    
+
     @FXML
     private TextField txtCantidadProductosASolicitar;
-    
+
     @FXML
-    void solicitarProductos(ActionEvent event){
-    	
-    	if (selectedItemList != null) {
-			
-    		int cantidadProducto = Integer.parseInt(txtCantidadProductosASolicitar.getText());
-    		empresa.agregarProductosSede(selectedItemList, cantidadProducto, administrador.getSede());
-    		
-    		txtCantidadProductosASolicitar.setText("");
-    		listProductosEmpresa.getSelectionModel().clearSelection();
-    		
+    void solicitarProductos(ActionEvent event) {
+
+        if (selectedItemList != null) {
+
+            try {
+                int cantidadProducto = Integer.parseInt(txtCantidadProductosASolicitar.getText());
+                empresa.agregarProductosSede(selectedItemList, cantidadProducto, administrador.getSede());
+
+                txtCantidadProductosASolicitar.setText("");
+                listProductosEmpresa.getSelectionModel().clearSelection();
+
+            } catch (StringNuloOrVacioException | SedeException | ProductoException e) {
+                e.printStackTrace();
+            }
+
 		}
-    	
+
     }
 
     @FXML
     void crearProducto(ActionEvent event) {
-    	
-    	String id = txtCodigoProducto.getText();
-    	String nombre = txtNombreProducto.getText();
-    	double precio = Double.parseDouble(txtPrecioProducto.getText());
-    	String descripcion = txtDescripcionProducto.getText();
-    	Image imagen = new Image(getClass().getResourceAsStream("../view/imagenesProyecto/" + txtNombreImagenProducto.getText()));
-    	int existencias = Integer.parseInt(txtCantidadProducto.getText());
-    	CategoriaProducto categoria = choiceBoxTipoProducto.getSelectionModel().getSelectedItem();
 
-    	try {
-    		
-			empresa.crearProducto(id, nombre, precio, descripcion, imagen, existencias, categoria);
-			
-			setearCamposDeTexto("", "", "", "", null, "");
-			
-		} catch (StringNuloOrVacioException | NegativeNumberException | ProductoException e) {
-			e.printStackTrace();
-		}
-    	
-    	listaProductosData.setAll(empresa.getListaProductos());
-    	
+        String id = txtCodigoProducto.getText();
+        String nombre = txtNombreProducto.getText();
+        double precio = Double.parseDouble(txtPrecioProducto.getText());
+        String descripcion = txtDescripcionProducto.getText();
+        Image imagen = new Image(getClass().getResourceAsStream("../view/imagenesProyecto/" + txtNombreImagenProducto.getText()));
+        int existencias = Integer.parseInt(txtCantidadProducto.getText());
+        CategoriaProducto categoria = choiceBoxTipoProducto.getSelectionModel().getSelectedItem();
+
+        try {
+
+            empresa.crearProducto(id, nombre, precio, descripcion, imagen, existencias, categoria);
+
+            setearCamposDeTexto("", "", "", "", null, "");
+
+        } catch (StringNuloOrVacioException | NegativeNumberException | ProductoException e) {
+            e.printStackTrace();
+        }
+
+        listaProductosData.setAll(empresa.getListaProductos());
+
     }
 
     @FXML
     void eliminarProducto(ActionEvent event) {
 
-    	if (selectedItemTable != null) {
-			
-    		try {
-    			
-				empresa.eliminarProducto(selectedItemTable.getId());
-				
-			} catch (StringNuloOrVacioException | ProductoException e) {
-				e.printStackTrace();
-			}	
-		}
-    	
-    	listaProductosData.setAll(empresa.getListaProductos());
+        if (selectedItemTable != null) {
+
+            try {
+
+                empresa.eliminarProducto(selectedItemTable.getId());
+
+            } catch (StringNuloOrVacioException | ProductoException e) {
+                e.printStackTrace();
+            }
+        }
+
+        listaProductosData.setAll(empresa.getListaProductos());
     }
 
     @FXML
     void actualizarProducto(ActionEvent event) {
 
-    	if (selectedItemTable != null) {
-			
-        	String nombre = txtNombreProducto.getText();
-        	double precio = Double.parseDouble(txtPrecioProducto.getText());
-        	String descripcion = txtDescripcionProducto.getText();
-        	Image imagen = new Image(getClass().getResourceAsStream("../view/imagenesProyecto/" + txtNombreImagenProducto.getText()));
-        	int existencias = Integer.parseInt(txtCantidadProducto.getText());
-        	CategoriaProducto categoria = choiceBoxTipoProducto.getSelectionModel().getSelectedItem();
-        	
-        	try {
-				empresa.actualizarProducto(selectedItemTable.getId(), nombre, precio, descripcion, imagen, existencias, categoria);
-				
-        	} catch (StringNuloOrVacioException | NegativeNumberException | ProductoException e) {
-				e.printStackTrace();
-			}
-		}
-    	
-    	listaProductosData.setAll(empresa.getListaProductos());
-    	
+        if (selectedItemTable != null) {
+
+            String nombre = txtNombreProducto.getText();
+            double precio = Double.parseDouble(txtPrecioProducto.getText());
+            String descripcion = txtDescripcionProducto.getText();
+            Image imagen = new Image(getClass().getResourceAsStream("../view/imagenesProyecto/" + txtNombreImagenProducto.getText()));
+            int existencias = Integer.parseInt(txtCantidadProducto.getText());
+            CategoriaProducto categoria = choiceBoxTipoProducto.getSelectionModel().getSelectedItem();
+
+            try {
+                empresa.actualizarProducto(selectedItemTable.getId(), nombre, precio, descripcion, imagen, existencias, categoria);
+
+            } catch (StringNuloOrVacioException | NegativeNumberException | ProductoException e) {
+                e.printStackTrace();
+            }
+        }
+
+        listaProductosData.setAll(empresa.getListaProductos());
+
     }
 
     @FXML
     void limpiarCampos(ActionEvent event) {
 
-    	setearCamposDeTexto("", "", "", "", null, "");
-    	
+        setearCamposDeTexto("", "", "", "", null, "");
+
     }
 
     @FXML
     void initialize() {
-    	
-    	listaProductosData.setAll(empresa.getListaProductos());
-        
-    	tblColCodigoProducto.setCellValueFactory(new PropertyValueFactory<>("id"));
-    	tblColNombreProducto.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-    	tblColPrecioProducto.setCellValueFactory(new PropertyValueFactory<>("precio"));
-    	tblColCantidadProducto.setCellValueFactory(new PropertyValueFactory<>("existencias"));
-    	tblColTipoProducto.setCellValueFactory(new PropertyValueFactory<>("categoria"));
-    	
-    	tblProducto.setItems(listaProductosData);
+
+        listaProductosData.setAll(empresa.getListaProductos());
+
+        tblColCodigoProducto.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblColNombreProducto.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        tblColPrecioProducto.setCellValueFactory(new PropertyValueFactory<>("precio"));
+        tblColCantidadProducto.setCellValueFactory(new PropertyValueFactory<>("existencias"));
+        tblColTipoProducto.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+
+        tblProducto.setItems(listaProductosData);
 
         //Función que se ejecuta cuando uno da click en una línea.
-    	tblProducto.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-    		
-    		selectedItemTable = newValue;
-    		if (selectedItemTable != null) {
-				
-    			setearCamposDeTexto(selectedItemTable.getId(), selectedItemTable.getNombre(), 
-    					Double.toString(selectedItemTable.getPrecio()), Integer.toString(selectedItemTable.getExistencias()), 
-    					selectedItemTable.getCategoria(), selectedItemTable.getDescripcion());
-    			
-			}
-    	});
-    	
-    	listProductosEmpresa.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
-    		
-    		if (newValue != null) {
-				
-    			selectedItemList = newValue;
-    			
-			}
-    	});
-    	
-    	choiceBoxTipoProducto.getItems().addAll(CategoriaProducto.values());
-    	
-    	txtCodigoProductoBuscar.textProperty().addListener((observable, oldValue, newValue) -> {
-    		
-    		if (newValue != "") {
-				
-    			listaProductosData.setAll(empresa.filtrarProductosPorCodigo(newValue));
-    			
-			}
-    		
-    	});
-    	
-    	listProductosEmpresa.getItems().setAll(listaProductosData);
-    	
+        tblProducto.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+
+            selectedItemTable = newValue;
+            if (selectedItemTable != null) {
+
+                setearCamposDeTexto(selectedItemTable.getId(), selectedItemTable.getNombre(),
+                        Double.toString(selectedItemTable.getPrecio()), Integer.toString(selectedItemTable.getExistencias()),
+                        selectedItemTable.getCategoria(), selectedItemTable.getDescripcion());
+
+            }
+        });
+
+        listProductosEmpresa.getSelectionModel().selectedItemProperty().addListener((obs, oldValue, newValue) -> {
+
+            if (newValue != null) {
+
+                selectedItemList = newValue;
+
+            }
+        });
+
+        choiceBoxTipoProducto.getItems().addAll(CategoriaProducto.values());
+
+        txtCodigoProductoBuscar.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (newValue != "") {
+
+                listaProductosData.setAll(empresa.filtrarProductosPorCodigo(newValue));
+
+            }
+
+        });
+
+        listProductosEmpresa.getItems().setAll(listaProductosData);
+
     }
 
-	private void setearCamposDeTexto(String id, String nombre, String precio, String existencias,
-			CategoriaProducto categoria, String descripcion) {
+    private void setearCamposDeTexto(String id, String nombre, String precio, String existencias,
+                                     CategoriaProducto categoria, String descripcion) {
 
-		txtCodigoProducto.setText(id);
-		txtNombreProducto.setText(nombre);
-		txtPrecioProducto.setText(precio);
-		txtCantidadProducto.setText(existencias);
-		choiceBoxTipoProducto.setValue(categoria);
-		txtDescripcionProducto.setText(descripcion);
-		txtNombreImagenProducto.setText("");
-		
-	}
+        txtCodigoProducto.setText(id);
+        txtNombreProducto.setText(nombre);
+        txtPrecioProducto.setText(precio);
+        txtCantidadProducto.setText(existencias);
+        choiceBoxTipoProducto.setValue(categoria);
+        txtDescripcionProducto.setText(descripcion);
+        txtNombreImagenProducto.setText("");
 
-	public void setearAdministrador(Administrador administrador) {
-		
-		this.administrador = administrador;
-	
-	}
+    }
+
+    public void setearAdministrador(Administrador administrador) {
+
+        this.administrador = administrador;
+
+    }
 }
